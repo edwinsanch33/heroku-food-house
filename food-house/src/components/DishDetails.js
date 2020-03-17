25411af6cdb56533ea73b40b07bf70c966d5b46a
Cece Claridge <cece.claridge@gmail.com>
2020-03-17 14:13:38 -0400

    added static API call in Dish Details

--- food-house/src/components/DishDetails.js
+++ food-house/src/components/DishDetails.js
@@ -3,16 +3,18 @@ import axios from 'axios';
 
 const DishDetails = (id) => {
 
+
+
     const [recipe, setRecipe] = useState({ meals: [] })
 
     const getRecipe = async () => {
-        const result = await axios(
+        const { data } = await axios(
           `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
         );
-        setRecipe({ meals: result.data.meals });
+        setRecipe({ meals: data.meals[0] });
       };
-     
-    
+ 
+
       useEffect(() => {
         getRecipe();
       }, []);
@@ -22,6 +24,7 @@ const DishDetails = (id) => {
     return (
         <div>
             <h1>About This Dish</h1>
+            <p>{recipe.meals.strMeal}</p>
         </div>
     )
 }
